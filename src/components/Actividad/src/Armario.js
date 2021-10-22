36ce107c03858df88da35ff1eb8b05cc9e5555db
NymaXx <53951684+NymaXx@users.noreply.github.com>
2021-10-22 12:14:10 -0500

    create classes

---
+++ src/components/Actividad/src/Armario.js
@@ -0,0 +1,13 @@
+class Armario{
+    constructor(code, posX, posY){
+        this.posX=posX;
+        this.posY=posY;
+        this.code=code;
+        this.tops = [];
+        this.bots = [];
+        this.shoes= [];
+
+    }
+}
+
+export default Armario;
---
+++ src/components/Actividad/src/Cliente.js
@@ -0,0 +1,14 @@
+class Cliente{
+    constructor(code, posX, posY, currentState){
+        this.code = code;
+        this.posX = posX;
+        this.posY = posY;
+        this.currentState = currentState;
+        this.armario = [];
+
+    }
+
+
+}
+
+export default Cliente;

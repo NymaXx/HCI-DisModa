f48bc42284cb863961d3e6543f2f7c86f58bf001
NymaXx <53951684+NymaXx@users.noreply.github.com>
2021-10-22 11:22:13 -0500

    simple screen change

--- src/components/Actividad/Actividad.tsx
+++ src/components/Actividad/Actividad.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import "./Actividad.scss";
 import ActividadContext, { ActividadContextProvider } from "./config/ActividadContext";
 import MEDIA from "./config/Routes";
+import Game from "./src/Game";
 
 const Actividad = () => {
     return <ActividadContextProvider>
@@ -15,7 +16,12 @@ const ActividadLoad = () => {
 
     useEffect(() => {
 
-      
+      //aqui va el codigo del test vocacional
+
+
+      actividad.addPROCESSING(new Game(actividad));
+
+
     }, [])
 
 
--- src/components/Actividad/src/Game.js
+++ src/components/Actividad/src/Game.js
@@ -1,18 +1,55 @@
+
 class Game {
 
-    constructor(actividad) {
+    constructor(actividad){
         this.actividad = actividad;
-        console.log("CLASE GAME CARGADA")
+        this.title = "hello puta";
+        this.screen1 = null;
+        this.screen2 = null; 
+        this.screen3 = null; 
+        this.screen4 = null;
+        this.screenStart = null;
+        this.currentScreen = 0;
+
     }
 
-    draw(app){
-        app.text("ESTE ES EL JUEGO", 100, 100)
-        app.rect(100,200, 100,100)
+    setup(app){
+        app.createCanvas(1250,600);
+        this.screen1= app.loadImage("./images/exterior.png");
+        this.screen2= app.loadImage("./images/tienda.png");
+        this.screen3= app.loadImage("./images/start.png");
+        this.screen4= app.loadImage("./images/vestier.png");
+        
     }
 
-    mousePressed(){
-        this.actividad.goName("JUEGO")
+    draw(app) {
+        app.background(0);
+        
+
+        switch(this.currentScreen){
+            case 0 :
+            app.image(this.screen3, 0,0, 1250,600);
+                break;
+
+            case 1 :
+            app.image(this.screen1, 0,0, 1250,600);
+                break;
+        };
+
+        app.textSize(20);
+        app.text(app.mouseX + 'X' + app.mouseY + 'Y', app.mouseX, app.mouseY);
     }
+
+    mousePressed(app){
+        switch(this.currentScreen){
+            case 0:
+                if(app.mouseX > 468 && app.mouseX < 768 && app.mouseY > 437 && app.mouseY < 505){
+                    this.currentScreen = 1;
+                }
+                break;
+        }
+    }
+
 }
 
 export default Game;
